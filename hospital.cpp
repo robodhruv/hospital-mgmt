1b1cf396b49bca0f6df7e9f3cb60a523cc1c551c
Shashwat Shukla <shashwatshukl@gmail.com>
2017-03-26 15:47:47 +0530

    Skeletal outline

---
+++ hospital.cpp
@@ -0,0 +1,54 @@
+/*
+Implementation of IITB Hospital in C++.
+This file contains declarations for all classes, structs and stl containers used.
+Function definitions will be enumerated in separate files. 
+*/
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <string>
+using namespace std;
+
+//Patients arrive and queue up at the reception.
+queue<patient> Reception;
+
+//Each patient has relevant attributes  
+class patient{
+	string name, id; //Identity of the patient
+	vector<string> symptoms; //Symptoms displayed by the patient. Change to stack if needed
+	stack<string> doctors; //Doctors the patient must visit based on his symptoms. Keep popping docs till empty
+	vector<diagnosis> prescription; //Diagnosed disease and corresponding treatement for the patient
+	public:
+	//Add accessor and mutator functions
+};
+
+//At the reception, the patients are assigned to a doctor based on their symptoms.
+//As of now, they are assigned to one doctor, despite having multiple symptoms. This doctor then 
+//assigns the patient to the next doctor based on his/her other symptoms.
+doctor assigndoc(patient p);
+
+//Each doctor is identified by his name, id(remove if not needed) and specialisation
+class doctor{
+	string name, id, field; //field refers to the doctor's specialisation
+	public:
+	diagnosis diagnose(patient p); //Diagnose patient p and prescribe a treatment
+	//Add accessor and mutator functions
+};
+
+//Doctors diagnose the patient to identify the disease and the corresponding treatment
+struct diagnosis{
+	string disease, treatment;
+};
+
+
+//The heart of the database. Each patient has one entry in the database.
+class patientrecord{
+	string name, id;
+	vector<diagnosis> record; //Contains the entire treatment history of the patient
+	public:
+	//Add accessor and mutator functions
+}
+
+//Followed by declarations for all the search related databases
