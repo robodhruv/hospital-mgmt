3a62c2ac7df9233ac5a2ecc83ced300181d385cb
Pranav Kulkarni <Pranav@Pranav.local>
2017-04-26 00:16:19 +0530

    Accessor-Mutator for Doc

--- hospital.h
+++ hospital.h
@@ -88,13 +88,22 @@ class doctor {
     queue<patient*> patientLine; // queue of the patient waiting to be diagnosed under the doctor.
     //..$%$%$ I am doubtful if this is how a queue of pointers to patient is implemeneted please check.$%$%$
 public:
+    //Add accessor and mutator functions
+    void doctor::setName(string &name);
+    void doctor::getName(string &name);
+    void doctor::setID(string &id);
+    void doctor::getID(string &id);
+    void doctor::setFieldID(string &id);
+    void doctor::getFieldID(string &id);
     void addToLine(patient * p); //adds patient p to the queue of the doctor.
     void diagnose(patient *p); //Diagnose patient first in the queue and add diagnosis in patient's prescription vector.
     int areUmyDoc(patient *p);
     vector<int> whatICanCure;
     void setWhatICanCure ();
-    //Add accessor and mutator functions
+
 };
+
+
 
 //Doctors diagnose the patient to identify the disease and the corresponding treatment
 class diagnosis {
